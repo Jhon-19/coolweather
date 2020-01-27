5d3a861d6b95de941d820aceaf21bd2eb6a65b7d
Jhon <1723368058@qq.com>
2020-01-27 17:22:24 +0800

    Second Commit

---
+++ app/src/main/java/com/example/jhon/coolweather/gson/AQI.java
@@ -0,0 +1,19 @@
+package com.example.jhon.coolweather.gson;
+
+/**
+ * Created by Jhon on 2020/1/27.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity
+    {
+
+        public String aqi;
+        public String pm25;
+
+    }
+
+}
---
+++ app/src/main/java/com/example/jhon/coolweather/gson/Now.java
@@ -0,0 +1,25 @@
+package com.example.jhon.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Jhon on 2020/1/27.
+ */
+
+public class Now {
+
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More
+    {
+
+        @SerializedName("txt")
+        public String info;
+
+    }
+
+}
