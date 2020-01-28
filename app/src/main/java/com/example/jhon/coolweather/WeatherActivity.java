20f02d45af670cdbe798065c061020dbb47ee8fa
Jhon <1723368058@qq.com>
2020-01-28 15:12:26 +0800

    增加后台自动更新天气

--- app/src/main/java/com/example/jhon/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/jhon/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.jhon.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.jhon.coolweather.gson.Forecast;
 import com.example.jhon.coolweather.gson.Weather;
+import com.example.jhon.coolweather.service.AutoUpdateService;
 import com.example.jhon.coolweather.util.HttpUtil;
 import com.example.jhon.coolweather.util.Utility;
 
@@ -242,6 +244,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent=new Intent(this, AutoUpdateService.class);
+        startService(intent);
 
     }
 }
